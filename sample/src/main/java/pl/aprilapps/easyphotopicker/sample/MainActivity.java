20db463a471b980c2e07fe03346b98a01567d57e
Jacek Kwiecień <jacek.kwiecien@schibsted.com>
2019-09-11 15:30:15 +0200

    Remove Nammu from sample and improve permissions handling

--- sample/src/main/java/pl/aprilapps/easyphotopicker/sample/MainActivity.java
+++ sample/src/main/java/pl/aprilapps/easyphotopicker/sample/MainActivity.java
@@ -7,25 +7,30 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import pl.aprilapps.easyphotopicker.ChooserType;
 import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
 import pl.aprilapps.easyphotopicker.MediaFile;
 import pl.aprilapps.easyphotopicker.MediaSource;
-import pl.tajchert.nammu.Nammu;
-import pl.tajchert.nammu.PermissionCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String PHOTOS_KEY = "easy_image_photos_list";
+    private static final int CHOOSER_PERMISSIONS_REQUEST_CODE = 7459;
+    private static final int CAMERA_REQUEST_CODE = 7500;
+    private static final int CAMERA_VIDEO_REQUEST_CODE = 7501;
+    private static final int GALLERY_REQUEST_CODE = 7502;
+    private static final int DOCUMENTS_REQUEST_CODE = 7503;
 
     protected RecyclerView recyclerView;
 
@@ -41,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Nammu.init(this);
 
         recyclerView = findViewById(R.id.recycler_view);
         galleryButton = findViewById(R.id.gallery_button);
@@ -54,21 +58,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(imagesAdapter);
-
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            Nammu.askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
-                @Override
-                public void permissionGranted() {
-                    //Nothing, this sample saves to Public gallery so it needs permission
-                }
-
-                @Override
-                public void permissionRefused() {
-                    finish();
-                }
-            });
-        }
 
         easyImage = new EasyImage.Builder(this)
                 .setChooserTitle("Pick media")
@@ -86,7 +75,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 /** Some devices such as Samsungs which have their own gallery app require write permission. Testing is advised! */
-                easyImage.openGallery(MainActivity.this);
+                String[] necessaryPermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openGallery(MainActivity.this);
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, GALLERY_REQUEST_CODE);
+                }
             }
         });
 
@@ -94,14 +88,24 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.camera_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                easyImage.openCameraForImage(MainActivity.this);
+                String[] necessaryPermissions = new String[]{Manifest.permission.CAMERA};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openCameraForImage(MainActivity.this);
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, CAMERA_REQUEST_CODE);
+                }
             }
         });
 
         findViewById(R.id.camera_video_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                easyImage.openCameraForVideo(MainActivity.this);
+                String[] necessaryPermissions = new String[]{Manifest.permission.CAMERA};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openCameraForVideo(MainActivity.this);
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, CAMERA_VIDEO_REQUEST_CODE);
+                }
             }
         });
 
@@ -109,22 +113,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 /** Some devices such as Samsungs which have their own gallery app require write permission. Testing is advised! */
-
-                int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                String[] necessaryPermissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (arePermissionsGranted(necessaryPermissions)) {
                     easyImage.openDocuments(MainActivity.this);
                 } else {
-                    Nammu.askForPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionCallback() {
-                        @Override
-                        public void permissionGranted() {
-                            easyImage.openDocuments(MainActivity.this);
-                        }
-
-                        @Override
-                        public void permissionRefused() {
-
-                        }
-                    });
+                    requestPermissionsCompat(necessaryPermissions, DOCUMENTS_REQUEST_CODE);
                 }
             }
         });
@@ -132,7 +125,12 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.chooser_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                easyImage.openChooser(MainActivity.this);
+                String[] necessaryPermissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                if (arePermissionsGranted(necessaryPermissions)) {
+                    easyImage.openChooser(MainActivity.this);
+                } else {
+                    requestPermissionsCompat(necessaryPermissions, CHOOSER_PERMISSIONS_REQUEST_CODE);
+                }
             }
         });
 
@@ -154,7 +152,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Nammu.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CHOOSER_PERMISSIONS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openChooser(MainActivity.this);
+        } else if (requestCode == CAMERA_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForImage(MainActivity.this);
+        } else if (requestCode == CAMERA_VIDEO_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openCameraForVideo(MainActivity.this);
+        } else if (requestCode == GALLERY_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openGallery(MainActivity.this);
+        } else if (requestCode == DOCUMENTS_REQUEST_CODE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            easyImage.openDocuments(MainActivity.this);
+        }
     }
 
     @Override
@@ -187,5 +196,18 @@ public class MainActivity extends AppCompatActivity {
         photos.addAll(Arrays.asList(returnedPhotos));
         imagesAdapter.notifyDataSetChanged();
         recyclerView.scrollToPosition(photos.size() - 1);
+    }
+
+    private boolean arePermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+                return false;
+
+        }
+        return true;
+    }
+
+    private void requestPermissionsCompat(String[] permissions, int requestCode) {
+        ActivityCompat.requestPermissions(MainActivity.this, permissions, requestCode);
     }
 }
